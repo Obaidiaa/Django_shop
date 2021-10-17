55f8b0030342777744b128b8185a4d6e74c92898
Abdulrhman Obaidi <obaidiaaa@gmail.com>
2021-10-17 21:13:17 +0300

    rename second tier apps folder as config

--- config/urls.py
+++ config/urls.py
@@ -1,4 +1,4 @@
-"""mysite URL Configuration
+"""config URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
@@ -16,9 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from mysite import views
+from config import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.index)
+    # path('',views.index)
 ]
