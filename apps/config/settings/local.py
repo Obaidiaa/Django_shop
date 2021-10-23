67df7aaaee071bea37269f248f178a683410ec8a
Abdulrhman Obaidi <obaidiaaa@gmail.com>
2021-10-23 23:21:53 +0300

    add django-livereload-server

--- apps/config/settings/local.py
+++ apps/config/settings/local.py
@@ -2,3 +2,11 @@ from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+
+
+MIDDLEWARE += (
+    'livereload.middleware.LiveReloadScript',
+)
+INSTALLED_APPS += [
+    'livereload',
+]
