635d970d9e3c7770c80c853f610a3844387eb2c9
Abdulrhman Obaidi <obaidiaaa@gmail.com>
2021-10-24 01:09:43 +0300

    finalize and test media and static file serving

--- apps/config/urls.py
+++ apps/config/urls.py
@@ -13,14 +13,21 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from os import stat
+from django.conf.urls import url,include
 from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
-
+from manage import get_env_variable
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'',include('main.urls')),
     # path('',views.index)
 ]
+
+DJANGO_EXECUTION_ENVIRONMENT = get_env_variable('DJANGO_EXECUTION_ENVIRONMENT')
+if DJANGO_EXECUTION_ENVIRONMENT == "LOCAL":
+    urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
